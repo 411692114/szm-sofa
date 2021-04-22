59dcd550c14dc6cf80f2a2057fe4117d582cc810
陈健波 <chenjianbo@guanfangyiliao.com>
2021-04-22 19:33:40 +0800

    配置ORM插件；优化配置项

--- szm-sofa-boot-starter-orm/src/main/java/com/sinszm/sofa/SofaBootStarterOrmConfiguration.java
+++ szm-sofa-boot-starter-orm/src/main/java/com/sinszm/sofa/SofaBootStarterOrmConfiguration.java
@@ -1,9 +1,16 @@
 package com.sinszm.sofa;
 
 
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 import com.sinszm.sofa.annotation.EnableORM;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
@@ -28,6 +35,9 @@ public class SofaBootStarterOrmConfiguration implements TransactionManagementCon
     @Resource
     private DataSource dataSource;
 
+    @Value("${spring.datasource.platform:other}")
+    private String dbType;
+
     @Bean(TRANSACTION_MANAGER)
     @Primary
     public PlatformTransactionManager mdbManager() {
@@ -44,6 +54,38 @@ public class SofaBootStarterOrmConfiguration implements TransactionManagementCon
     @Override
     public PlatformTransactionManager annotationDrivenTransactionManager() {
         return mdbManager;
+    }
+
+    /**
+     * 分页插件属性配置
+     * @return  拦截器
+     */
+    private PaginationInnerInterceptor paginationInnerInterceptor() {
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setOverflow(false);
+        paginationInnerInterceptor.setMaxLimit(500L);
+        paginationInnerInterceptor.setDbType(DbType.getDbType("all".equals(dbType) ? "other" : dbType));
+        return paginationInnerInterceptor;
+    }
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        //分页
+        interceptor.addInnerInterceptor(paginationInnerInterceptor());
+        //乐观锁
+        interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+        //防止全表更新与删除
+        interceptor.addInnerInterceptor(new BlockAttackInnerInterceptor());
+        return interceptor;
+    }
+
+    @Bean
+    public ConfigurationCustomizer configurationCustomizer() {
+        return configuration -> {
+            configuration.setUseGeneratedShortKey(true);
+            configuration.setUseDeprecatedExecutor(false);
+        };
     }
 
 }
