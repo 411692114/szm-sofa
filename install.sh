3a8d7f58ecdaa69217a792b337375b0dd615ec5c
陈健波 <chenjianbo@sinszm.com>
2021-04-23 09:28:42 +0800

    新增发布脚本

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# shellcheck disable=SC2155
+export GPG_TTY=$(tty)
+
+# 更新代码
+git pull
+
+# 发布
+mvn clean -pl .,szm-sofa-boot-starter,szm-sofa-boot-starter-orm install
+
+#清理
+mvn clean
