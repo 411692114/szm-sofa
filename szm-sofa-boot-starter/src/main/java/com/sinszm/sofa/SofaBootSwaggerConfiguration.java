f53d1fe1949357f6bc3ef0bbb019885b4f35004f
陈健波 <chenjianbo@guanfangyiliao.com>
2021-05-26 10:51:40 +0800

    解决增强模式未生效问题

--- szm-sofa-boot-starter/src/main/java/com/sinszm/sofa/SofaBootSwaggerConfiguration.java
+++ szm-sofa-boot-starter/src/main/java/com/sinszm/sofa/SofaBootSwaggerConfiguration.java
@@ -1,7 +1,9 @@
 package com.sinszm.sofa;
 
 import cn.hutool.core.util.StrUtil;
+import com.github.xiaoymin.knife4j.spring.extension.OpenApiExtensionResolver;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
@@ -36,11 +38,17 @@ public class SofaBootSwaggerConfiguration {
     @Resource
     private ApplicationContext applicationContext;
 
+    @Value("${knife4j.enable:false}")
+    private boolean enableKnife4j;
+
+    @Resource
+    private ApplicationContext context;
+
     @Bean
     public Docket createRestApi() {
         String groupName = applicationContext.getEnvironment()
                 .getProperty("spring.application.name");
-        return new Docket(DocumentationType.OAS_30)
+        Docket docket = new Docket(DocumentationType.OAS_30)
                 .enable(swagger3Properties.isEnable())
                 .groupName(
                         StrUtil.isEmpty(groupName) ? DEFAULT_GROUP_NAME : StrUtil.trimToEmpty(groupName)
@@ -50,6 +58,12 @@ public class SofaBootSwaggerConfiguration {
                 .apis(RequestHandlerSelectors.withMethodAnnotation(ApiOperation.class))
                 .paths(PathSelectors.any())
                 .build();
+        if (!enableKnife4j) {
+            return docket;
+        }
+        return docket.extensions(context.getBean(OpenApiExtensionResolver.class).buildExtensions(
+                StrUtil.isEmpty(groupName) ? DEFAULT_GROUP_NAME : StrUtil.trimToEmpty(groupName)
+        ));
     }
 
     private ApiInfo apiInfo() {
